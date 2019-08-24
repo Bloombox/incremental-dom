0966c11b1049d06c9c307254144d5226626937c2
Sam Gammon <sam@bloombox.io>
2019-08-23 21:42:25 -0700

    Fixup/rewrite for normal closure modules

---
+++ dist/closure/index.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2018 The Incremental DOM Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+goog.module('incrementaldom');
+goog.require('incrementaldom.src.attributes');
+goog.require('incrementaldom.src.core');
+goog.require('incrementaldom.src.global');
+goog.require('incrementaldom.src.node_data');
+goog.require('incrementaldom.src.notifications');
+goog.require('incrementaldom.src.symbols');
+goog.require('incrementaldom.src.virtual_elements');
